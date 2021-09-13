bf135a71b46aec790e5851ad8a23186254f89dbd
kfrost <85587872+kfrost-lab@users.noreply.github.com>
2021-09-13 00:22:29 +0000

    start version 2

---
+++ Quiz2.py
@@ -0,0 +1,31 @@
+import random
+
+print ("Welcome to your maths quiz")
+score=0
+
+for i in range(1,11):
+    symbols = ['+' , '-' , 'x']
+    num1 = random.randint(1,10)
+    num2 = random.randint(1,10)
+    question = random.choice(symbols)
+    math = eval(str(num1) + question + str(num2))
+    print ("The Question is" ,num1,question,num2)
+    
+    q= int(input("what is your answer:"))
+    if q==math:
+        print ("correct")
+        score = score + 1
+        print ("your total score is")
+        print (score)
+
+    else:
+        print ("incorrect, the answer was" ,math)
+
+
+if (score1) > 7:
+    print ("Great job you passed the test")
+        
+else:
+    print ("Sorry, you failed the test")
+        
+print ("Thanks for playing, See you next time")
